ee179c199846dd6acfd2ed121990b97758d7c067
yohay <yohay13@gmail.com>
2017-09-26 14:25:03 +0300

    first init

---
+++ index.js
@@ -0,0 +1,55 @@
+//q3
+function printTwistShout(){
+  for(var i = 1; i <= 100; i ++){
+    if(i % 3 == 0 && i % 5 == 0){
+      console.log("Twist & Shout");
+    }else if(i % 3 == 0){
+      console.log("Twist");
+    }else if(i % 5 == 0){
+      console.log("Shout");
+    }else{
+      console.log(i);
+    }
+  }
+}
+
+//q1
+//given [[],[1,2,3],[4,5],[],[],[6],[7,8],[],[9],[10],[]]
+function printNormal(arr){
+  for (var i = 0; i < arr.length; i ++){
+    for(var j = 0; j < arr[i].length; j ++){
+      console.log(arr[i][j]);
+    }
+  } 
+}
+
+//q2
+//given [[],[1,2,3],[4,5],[],[],[6],[7,8],[],[9],[10],[]]
+function printNormalOdds(arr){
+  for (var i = 0; i < arr.length; i ++){
+    for(var j = arr[i].length - 1; j >= 0 ; j --){
+      if(arr[i][j] % 2 == 0){
+        arr[i].splice(j, 1);
+      }
+    }
+  } 
+  printNormal(arr); 
+}
+
+//q4
+function countSpaces(str){
+  return countSpacesHelper(0, str);
+}
+// Helper function , could also be declared from countSpaces inner scope, but exposing it can make it reuseable
+function countSpacesHelper(index, str){
+  if(index  === str.length){
+    return 0;
+  }
+
+  if(str.charAt(index) == " "){
+    return countSpacesHelper(index + 1, str) + 1;
+  }
+  return countSpacesHelper(index + 1, str) + 0;
+}
+
+//q5
